b507f4e7b0bca9f89493f35c1df03bd0a14a94f9
JD Godchaux <jd@nijel.org>
2015-01-28 17:49:55 -0500

    Fun with Basic JS

---
+++ script.js
@@ -0,0 +1,76 @@
+// This is what a comment looks like
+// only people that look at your code with see this
+// the computer will ignore it entierly
+
+// let's do some simple math
+var number = 18 + 27;
+console.log(number);
+
+//confirm("This is the best website ever!");
+
+//prompt("Why are we here?");
+
+//var anotherNumber = 42;
+//console.log(anotherNumber);
+
+var aString = "This is a string.";
+//console.log(aString);
+
+// how long is the string?
+//aString.length;
+
+// true or false?
+//var isThisTrue = aString.length < 6;
+//console.log(isThisTrue);
+
+
+// comparisons
+
+//greater than
+//isThisTrue = aString.length > 6;
+//console.log(isThisTrue);
+
+// less than
+//isThisTrue = aString.length < 42;
+//console.log(isThisTrue);
+
+// less than or equal to
+//isThisTrue = aString.length <= 42;
+//console.log(isThisTrue);
+
+// greater than or equal to
+//isThisTrue = aString.length >= 42;
+//console.log(isThisTrue);
+
+// completely equal to
+//isThisTrue = aString.length === "This is a string";
+//console.log(isThisTrue);
+
+// not equal to
+//isThisTrue = aString.length !== "This is a string";
+//console.log(isThisTrue);
+
+// basic if statements
+/*
+if (aString.length !== "This is a string") {
+  // do this
+  console.log('do this');
+} else {
+  // do that
+  console.log('do that');
+}
+*/
+
+// if you don't want to do anything if the if statement is true, then you can leave the else off
+/*
+if (aString.length !== "This is a string") {
+  // do this
+  console.log('do this');
+}
+*/
+
+
+
+
+
+
