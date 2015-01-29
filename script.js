a41b30ee7a7d8572b48331c6756d172924f6c614
JD Godchaux <jd@nijel.org>
2015-01-28 21:33:12 -0500

    class changes

--- script.js
+++ script.js
@@ -3,71 +3,70 @@
 // the computer will ignore it entierly
 
 // let's do some simple math
-var number = 18 + 27;
+var number = 18 + 22;
 console.log(number);
 
 //confirm("This is the best website ever!");
 
 //prompt("Why are we here?");
 
-//var anotherNumber = 42;
-//console.log(anotherNumber);
+var anotherNumber = 42;
+console.log(anotherNumber);
 
 var aString = "This is a string.";
-//console.log(aString);
+console.log(aString);
 
 // how long is the string?
-//aString.length;
+//console.log(aString.length);
 
 // true or false?
-//var isThisTrue = aString.length < 6;
-//console.log(isThisTrue);
+var isThisTrue = aString.length < 6;
+console.log(isThisTrue);
 
 
 // comparisons
 
 //greater than
-//isThisTrue = aString.length > 6;
-//console.log(isThisTrue);
+isThisTrue = aString.length > 6;
+console.log(isThisTrue);
 
 // less than
-//isThisTrue = aString.length < 42;
-//console.log(isThisTrue);
+isThisTrue = aString.length < 42;
+console.log(isThisTrue);
 
 // less than or equal to
-//isThisTrue = aString.length <= 42;
-//console.log(isThisTrue);
+isThisTrue = aString.length <= 42;
+console.log(isThisTrue);
 
 // greater than or equal to
-//isThisTrue = aString.length >= 42;
-//console.log(isThisTrue);
+isThisTrue = aString.length >= 42;
+console.log(isThisTrue);
 
 // completely equal to
-//isThisTrue = aString.length === "This is a string";
-//console.log(isThisTrue);
+isThisTrue = anotherNumber === 42;
+console.log("completely equal to", isThisTrue);
 
 // not equal to
-//isThisTrue = aString.length !== "This is a string";
+isThisTrue = aString !== "This is a string.";
 //console.log(isThisTrue);
 
 // basic if statements
-/*
-if (aString.length !== "This is a string") {
+
+if (anotherNumber !== 42) {
   // do this
   console.log('do this');
 } else {
   // do that
   console.log('do that');
 }
-*/
 
 // if you don't want to do anything if the if statement is true, then you can leave the else off
-/*
-if (aString.length !== "This is a string") {
+
+if (anotherNumber == 42) {
   // do this
-  console.log('do this');
+  console.log('yea!');
 }
-*/
+
 
 
 
